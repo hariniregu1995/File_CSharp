c855f3cea45ba1fd8fbdb73c109b4b04ecdf93ff
hariniregu1995 <98214414+hariniregu1995@users.noreply.github.com>
2024-12-17 19:46:17 +0530

    Add project files.

---
+++ File_C#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        // Create and write to a file
+        //File.WriteAllText(@"C:\MyFiles\example.txt", "Hello, World!");
+        //File.WriteAllText("sample.txt", "Hello, File Handling!");
+        // Read the file
+        //string content = File.ReadAllText("sample.txt");
+        // Console.WriteLine("File Content: " + content);
+        Directory.CreateDirectory("MyFolder");
+    }
+}
